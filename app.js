3d164cdd3c464d6bbc59980597b826579d1c9634
Sachin <007mishrasachinmishra@gmail.com>
2024-09-23 20:11:37 +0530

    updated projects & navbar

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const hamburger = document.querySelector("#hamburger")
-const menu = document.querySelector("#menu")
-const hlink = document.querySelectorAll("#hlink")
-hamburger.addEventListener("click",()=>{
-    menu.classList.toggle("hidden")
-    hamburger.classList.toggle("bg-white")
-})
+// const hamburger = document.querySelector("#hamburger")
+// const menu = document.querySelector("#menu")
+// const hlink = document.querySelectorAll("#hlink")
+// hamburger.addEventListener("click",()=>{
+//     menu.classList.toggle("hidden")
+//     hamburger.classList.toggle("bg-white")
+// })
+const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
+    });
 hlink.forEach(link=>{
     link.addEventListener("click",()=>{
         menu.classList.toggle("hidden")
